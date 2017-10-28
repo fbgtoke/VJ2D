26b99ab11060c60d8b44d0501d1055e1af973631
itsshowtime <kilian.pe.co@gmail.com>
2017-10-28 08:27:32 +0200

    added bombs and crates, made a few 'new' levels

---
+++ CMakeFiles/Bubble.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kilian/Downloads/VJ/VJ2D/BubbleBoard.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/BubbleBoard.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/BubbleLevel.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/BubbleLevel.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Cannon.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Cannon.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/FallingBubble.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/FallingBubble.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Game.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Game.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/MovingBubble.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/MovingBubble.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Particle.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Particle.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Scene.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Scene.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SceneCredits.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SceneCredits.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SceneGameOver.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SceneGameOver.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SceneHelp.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SceneHelp.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SceneMenu.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SceneMenu.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/ScenePlay.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/ScenePlay.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SceneWin.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SceneWin.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Shader.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Shader.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/ShaderProgram.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/ShaderProgram.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Sprite.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Sprite.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SpriteBobble.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SpriteBobble.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/SpriteBubble.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/SpriteBubble.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Text.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Text.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/Texture.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/Texture.cpp.o"
+  "/home/kilian/Downloads/VJ/VJ2D/main.cpp" "/home/kilian/Downloads/VJ/VJ2D/CMakeFiles/Bubble.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SOIL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Bubble.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bubble.dir/BubbleBoard.cpp.o"
+  "CMakeFiles/Bubble.dir/BubbleLevel.cpp.o"
+  "CMakeFiles/Bubble.dir/Cannon.cpp.o"
+  "CMakeFiles/Bubble.dir/FallingBubble.cpp.o"
+  "CMakeFiles/Bubble.dir/MovingBubble.cpp.o"
+  "CMakeFiles/Bubble.dir/Particle.cpp.o"
+  "CMakeFiles/Bubble.dir/Sprite.cpp.o"
+  "CMakeFiles/Bubble.dir/SpriteBobble.cpp.o"
+  "CMakeFiles/Bubble.dir/SpriteBubble.cpp.o"
+  "CMakeFiles/Bubble.dir/Texture.cpp.o"
+  "CMakeFiles/Bubble.dir/Scene.cpp.o"
+  "CMakeFiles/Bubble.dir/SceneCredits.cpp.o"
+  "CMakeFiles/Bubble.dir/SceneGameOver.cpp.o"
+  "CMakeFiles/Bubble.dir/SceneHelp.cpp.o"
+  "CMakeFiles/Bubble.dir/SceneMenu.cpp.o"
+  "CMakeFiles/Bubble.dir/ScenePlay.cpp.o"
+  "CMakeFiles/Bubble.dir/SceneWin.cpp.o"
+  "CMakeFiles/Bubble.dir/Shader.cpp.o"
+  "CMakeFiles/Bubble.dir/ShaderProgram.cpp.o"
+  "CMakeFiles/Bubble.dir/Text.cpp.o"
+  "CMakeFiles/Bubble.dir/Game.cpp.o"
+  "CMakeFiles/Bubble.dir/main.cpp.o"
+  "Bubble.pdb"
+  "Bubble"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bubble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
